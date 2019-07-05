3cb4e0c509383095c0af5fe700ae04a7b68b9765
eHime <kinuyamakomaba@gmail.com>
2019-07-05 18:32:22 +0900

    ボタンは置いてる、リファクタリングもしてる、documentIdの書き直しがまだ、といったところ。ここから続き始めていきます。（Gitはマジで完璧にマスターできた）

--- Firestore1.self/Constants/Constants.swift
+++ Firestore1.self/Constants/Constants.swift
@@ -6,6 +6,12 @@
 //  Copyright © 2019 関口大海. All rights reserved.
 //
 
+/*
+ ボタンは置いてる、リファクタリングもしてる、documentIdの書き直しがまだ、といったところ。
+ ここから続き始めていきます。
+ 
+ */
+
 import Foundation
 
 // 打ち間違えなどのミスに備えて。（StringでFirebase内のkey値を指定すると、Xcodeでサジェストが出ないので。）
