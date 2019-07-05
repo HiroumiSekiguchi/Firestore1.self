199b00ac1ad71b9b6e2734ab64d3b63bc96121cb
eHime <kinuyamakomaba@gmail.com>
2019-07-05 19:33:28 +0900

    諸々のバグはあるけど、とりあえず一旦ひと段落とする。未解決問題がいくつかあるので、それは「memo」ファイル内に記した。ここから先はそれらの解決にあたるべく動いていくものとする。

---
+++ Firestore1.self/Constants/memo.swift
@@ -0,0 +1,25 @@
+//
+//  memo.swift
+//  Firestore1.self
+//
+//  Created by 関口大海 on 2019/07/05.
+//  Copyright © 2019 関口大海. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 以下が未解決の箇所
+ ・funny, serious, crazyのクリック問題
+ ・setListner()内コードのリファクタリングがバグってたので元に戻した（原因特定して正常に機能させたい...）
+ ・セルのreusable問題を解決
+ ・checkmarkの付け外しをタップで（セル内のデータにその情報を格納、上書きするように）
+ ・Dateが投稿取得時のものにならない
+ ・Firebaseからデータを取得する時のやり方について研究
+ ・Firebaseにデータを上書きする方法も（モデル内にしっかりdocumentIdの指定を入れる！）
+ ・Imageをボタンにする方法について（まぁ自力で実装はできなくてもいいのでは）
+ ・セルの削除機能、編集機能とかもつけたい
+ */
+
+
+
--- Firestore1.self/Models/Post.swift
+++ Firestore1.self/Models/Post.swift
@@ -32,28 +32,28 @@ class Post {
     }
     
     // クラスメソッドでリファクタリング（何処かが間違っている！）
-    class func parseData(snapshot: QuerySnapshot?) -> [Post] {
-        guard let snap = snapshot else { return postArray }
-        for document in snap.documents {
-            
-            let data = document.data()
-            
-            let title = data[TITLE] as? String ?? "タイトルなし"
-            let content = data[CONTENT] as? String ?? "内容なし"
-            let numLikes = data[NUM_LIKES] as? Int ?? 0
-            let category = data[CATEGORY] as? String ?? PostCategory.funny.rawValue
-            let timestamp = data[TIMESTAMP] as? Date ?? Date()
-            let documentId = document.documentID
-            
-            
-            // 上記に基づいたPostクラスのインスタンスを生成
-            let newPost = Post(category: category, title: title, content: content, numLikes: numLikes, timestamp: timestamp, documentId: documentId)
-            
-            // 上記を配列に追加
-            postArray.append(newPost)
-        }
-        return postArray
-    }
+//    class func parseData(snapshot: QuerySnapshot?) -> [Post] {
+//        guard let snap = snapshot else { return postArray }
+//        for document in snap.documents {
+//
+//            let data = document.data()
+//
+//            let title = data[TITLE] as? String ?? "タイトルなし"
+//            let content = data[CONTENT] as? String ?? "内容なし"
+//            let numLikes = data[NUM_LIKES] as? Int ?? 0
+//            let category = data[CATEGORY] as? String ?? PostCategory.funny.rawValue
+//            let timestamp = data[TIMESTAMP] as? Date ?? Date()
+//            let documentId = document.documentID
+//
+//
+//            // 上記に基づいたPostクラスのインスタンスを生成
+//            let newPost = Post(category: category, title: title, content: content, numLikes: numLikes, timestamp: timestamp, documentId: documentId)
+//
+//            // 上記を配列に追加
+//            postArray.append(newPost)
+//        }
+//        return postArray
+//    }
     
 }
 
